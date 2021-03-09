2b07433c1f3cb7627507e288ab1caeb08bbf044b
Mathias Gottschlag <mgottschlag@gmail.com>
2021-03-09 07:59:32 +0100

    firmware: New zephyr project.

--- firmware/main/compile.sh
+++ firmware/main/compile.sh
@@ -1,30 +1,4 @@
 #!/bin/bash
 
-set -e
+west build -p auto -b adafruit_feather_nrf52840
 
-# compile the main application
-mkdir -p build
-cd build
-if [ ! -f Makefile ]
-then
-	cmake .. -DCMAKE_TOOLCHAIN_FILE=../Toolchain.cmake
-fi
-make -j`nproc`
-
-# regenerate the documentation to show documentation warnings
-(
-	cd ..
-	doxygen > /dev/null
-)
-
-# show the size of the application
-arm-none-eabi-size goboard-main
-
-# generate a hex from the main application
-arm-none-eabi-objcopy -O ihex goboard-main goboard-main.hex
-
-# NOTE: use "uf2conv.py goboard-main.hex -c -f 0xADA52840" to generate an UF2 file
-adafruit-nrfutil dfu genpkg --dev-type 0x0052 --application goboard-main.hex goboard-main.zip
-
-# NOTE: use the following command to flash via the adafruit DFU bootloader
-# adafruit-nrfutil dfu serial --package build/goboard-main.zip -p /dev/ttyACM0
---
+++ firmware/main/src/main.cpp
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr.h>
+#include <device.h>
+#include <devicetree.h>
+#include <drivers/gpio.h>
+
+/* 1000 msec = 1 sec */
+#define SLEEP_TIME_MS   1000
+
+#define LED0_NODE DT_ALIAS(led0)
+
+#if !DT_NODE_HAS_STATUS(LED0_NODE, okay)
+#error "devicetree has no led0 node"
+#endif
+
+#define LED0 DT_GPIO_LABEL(LED0_NODE, gpios)
+#define PIN DT_GPIO_PIN(LED0_NODE, gpios)
+#define FLAGS DT_GPIO_FLAGS(LED0_NODE, gpios)
+
+void main(void) {
+	const struct device *dev;
+	bool led_is_on = true;
+	int ret;
+
+	dev = device_get_binding(LED0);
+	if (dev == NULL) {
+		return;
+	}
+
+	ret = gpio_pin_configure(dev, PIN, GPIO_OUTPUT_ACTIVE | FLAGS);
+	if (ret < 0) {
+		return;
+	}
+
+	while (1) {
+		gpio_pin_set(dev, PIN, (int)led_is_on);
+		led_is_on = !led_is_on;
+		k_msleep(SLEEP_TIME_MS);
+	}
+}
+
