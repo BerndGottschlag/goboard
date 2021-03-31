ddcca855d80990c5f50deecbad3b55bc598127f6
Mathias Gottschlag <mathias.gottschlag@kit.edu>
2021-03-31 18:33:14 +0200

    Main: Firmware: Implement the mode switch.

--- firmware/main/src/mode_switch.cpp
+++ firmware/main/src/mode_switch.cpp
@@ -2,100 +2,174 @@
 
 #include "exception.hpp"
 
-#define MODESW0_NODE DT_ALIAS(modesw0)
-#define MODESW1_NODE DT_ALIAS(modesw1)
+#include <hal/nrf_gpio.h>
 
-#if !DT_NODE_HAS_STATUS(MODESW0_NODE, okay) || !DT_NODE_HAS_STATUS(MODESW1_NODE, okay)
-//#error No mode switch defined in device tree, did you select the right board?
-#endif
+#define MODESW0 DT_ALIAS(modesw0)
+#define MODESW1 DT_ALIAS(modesw1)
 
+#define MODESW0_LABEL DT_GPIO_LABEL(MODESW0, gpios)
+#define MODESW0_PIN   DT_GPIO_PIN(MODESW0, gpios)
+#define MODESW0_FLAGS (GPIO_INPUT | DT_GPIO_FLAGS(MODESW0, gpios))
 
-#define MODESW0_GPIO_LABEL DT_GPIO_LABEL(MODESW0_NODE, gpios)
-#define MODESW0_GPIO_PIN   DT_GPIO_PIN(MODESW0_NODE, gpios)
-#define MODESW0_GPIO_FLAGS (GPIO_INPUT | DT_GPIO_FLAGS(MODESW0_NODE, gpios))
+#define MODESW1_LABEL DT_GPIO_LABEL(MODESW1, gpios)
+#define MODESW1_PIN   DT_GPIO_PIN(MODESW1, gpios)
+#define MODESW1_FLAGS (GPIO_INPUT | DT_GPIO_FLAGS(MODESW1, gpios))
 
-#define MODESW1_GPIO_LABEL DT_GPIO_LABEL(MODESW1_NODE, gpios)
-#define MODESW1_GPIO_PIN   DT_GPIO_PIN(MODESW1_NODE, gpios)
-#define MODESW1_GPIO_FLAGS (GPIO_INPUT | DT_GPIO_FLAGS(MODESW1_NODE, gpios))
+#define DEBOUNCE_INTERVAL K_MSEC(200)
 
 ModeSwitch::ModeSwitch() {
-	/*const struct device *sw0 = device_get_binding(MODESW0_GPIO_LABEL);
-	if (sw0 == NULL) {
+	// Initialize the GPIOs.
+	sw0_gpio = device_get_binding(MODESW0_LABEL);
+	if (sw0_gpio == NULL) {
 		throw InitializationFailed("Did not find modesw0.");
 	}
-	const struct device *sw1 = device_get_binding(MODESW1_GPIO_LABEL);
-	if (sw1 == NULL) {
+	sw1_gpio = device_get_binding(MODESW1_LABEL);
+	if (sw1_gpio == NULL) {
 		throw InitializationFailed("Did not find modesw0.");
 	}
 
-	int ret = gpio_pin_configure(sw0, MODESW0_GPIO_PIN, MODESW0_GPIO_FLAGS);
+	int ret = gpio_pin_configure(sw0_gpio, MODESW0_PIN, MODESW0_FLAGS);
 	if (ret != 0) {
 		throw InitializationFailed("Failed to configure modesw0.");
 	}
-	ret = gpio_pin_configure(sw1, MODESW1_GPIO_PIN, MODESW1_GPIO_FLAGS);
+	ret = gpio_pin_configure(sw1_gpio, MODESW1_PIN, MODESW1_FLAGS);
 	if (ret != 0) {
 		throw InitializationFailed("Failed to configure modesw1.");
 	}
 
-	ret = gpio_pin_interrupt_configure(sw0,
-	                                   MODESW0_GPIO_PIN,
+	// The change callback is delayed to debounce the switch and to generate
+	// clean transitions between the switch positions.
+	k_work_init_delayable(&callback, static_on_callback);
+
+	// Initialize the GPIO interrupts.
+	ret = gpio_pin_interrupt_configure(sw0_gpio,
+	                                   MODESW0_PIN,
 	                                   GPIO_INT_EDGE_TO_ACTIVE);
 	if (ret != 0) {
 		throw InitializationFailed("Failed to configure modesw0 interrupt.");
 	}
-	ret = gpio_pin_interrupt_configure(sw1,
-	                                   MODESW1_GPIO_PIN,
-	                                   GPIO_INT_EDGE_TO_ACTIVE);
+	ret = gpio_pin_interrupt_configure(sw1_gpio,
+	                                   MODESW1_PIN,
+	                                   GPIO_INT_EDGE_BOTH);
 	if (ret != 0) {
 		throw InitializationFailed("Failed to configure modesw1 interrupt.");
 	}
 
-	gpio_init_callback(&this->sw0_cb_data,
+	gpio_init_callback(&sw0_cb_data,
 	                   ModeSwitch::sw0_gpio_callback,
-	                   BIT(MODESW0_GPIO_PIN));
-	gpio_add_callback(sw0, &this->sw0_cb_data);
-	gpio_init_callback(&this->sw1_cb_data,
+	                   BIT(MODESW0_PIN));
+	gpio_add_callback(sw0_gpio, &sw0_cb_data);
+	gpio_init_callback(&sw1_cb_data,
 	                   ModeSwitch::sw1_gpio_callback,
-	                   BIT(MODESW1_GPIO_PIN));
-	gpio_add_callback(sw1, &this->sw1_cb_data);*/
-
-	// TODO
+	                   BIT(MODESW1_PIN));
+	gpio_add_callback(sw1_gpio, &sw1_cb_data);
 }
 
 ModeSwitch::~ModeSwitch() {
-	// TODO
+	int position = get_position();
+	// Disable the interrupts - the callback must not called after this
+	// object has been deallocated.
+	gpio_add_callback(sw0_gpio, &sw0_cb_data);
+	gpio_add_callback(sw1_gpio, &sw1_cb_data);
+	gpio_pin_interrupt_configure(sw0_gpio,
+	                             MODESW0_PIN,
+	                             GPIO_INT_DISABLE);
+	gpio_pin_interrupt_configure(sw1_gpio,
+	                             MODESW1_PIN,
+	                             GPIO_INT_DISABLE);
+
+	// Prevent any further callbacks.
+	k_work_sync sync;
+	k_work_cancel_delayable_sync(&callback, &sync);
+
+	// Configure the switch to wake the system up.
+	nrf_gpio_cfg_input(MODESW0_PIN, NRF_GPIO_PIN_NOPULL);
+	nrf_gpio_cfg_input(MODESW1_PIN, NRF_GPIO_PIN_NOPULL);
+	if (position == 0) {
+		nrf_gpio_cfg_sense_set(MODESW0_PIN, NRF_GPIO_PIN_SENSE_HIGH);
+		nrf_gpio_cfg_sense_set(MODESW1_PIN, NRF_GPIO_PIN_SENSE_HIGH);
+	} else if (position == 1) {
+		nrf_gpio_cfg_sense_set(MODESW0_PIN, NRF_GPIO_PIN_SENSE_LOW);
+		nrf_gpio_cfg_sense_set(MODESW1_PIN, NRF_GPIO_PIN_SENSE_HIGH);
+	} else {
+		nrf_gpio_cfg_sense_set(MODESW0_PIN, NRF_GPIO_PIN_SENSE_HIGH);
+		nrf_gpio_cfg_sense_set(MODESW1_PIN, NRF_GPIO_PIN_SENSE_LOW);
+	}
+
 }
 
 KeyboardMode ModeSwitch::get_mode() {
-	// TODO
-	return MODE_OFF_USB;
+	// TODO: Make the radio mode configurable.
+	int position = get_position();
+	switch (position) {
+	case 1:
+		return MODE_BLUETOOTH;
+	case 2:
+		return MODE_BLUETOOTH;
+	default:
+		return MODE_OFF_USB;
+	}
 }
 
 KeyboardProfile ModeSwitch::get_profile() {
-	// TODO
-	return PROFILE_1;
+	int position = get_position();
+	switch (position) {
+	case 2:
+		return PROFILE_2;
+	default:
+		return PROFILE_1;
+	}
 }
 
 void ModeSwitch::set_callback(void (*change_callback)()) {
-	(void)change_callback;
-	// TODO
+	k_sched_lock();
+	callback_fn = change_callback;
+	k_sched_unlock();
 }
 
-void ModeSwitch::sw0_gpio_callback(struct device *port, struct gpio_callback *cb, uint32_t pins) {
+unsigned int ModeSwitch::get_position() {
+	int status = gpio_pin_get(sw0_gpio, MODESW0_PIN);
+	if (status < 0) {
+		throw HardwareError("failed to get mode switch state");
+	} else if (status) {
+		return 1;
+	}
+	status = gpio_pin_get(sw1_gpio, MODESW1_PIN);
+	if (status < 0) {
+		throw HardwareError("failed to get mode switch state");
+	} else if (status) {
+		return 2;
+	}
+
+	return 0;
+}
+
+void ModeSwitch::sw0_gpio_callback(const struct device *port, struct gpio_callback *cb, uint32_t pins) {
 	(void)port;
 	(void)pins;
 	ModeSwitch *ms = CONTAINER_OF(cb, ModeSwitch, sw0_cb_data);
-	ms->gpio_callback();
+	// Debounce the switch by waiting for some time before calling the
+	// callback.
+	k_work_schedule(&ms->callback, DEBOUNCE_INTERVAL);
 }
 
-void ModeSwitch::sw1_gpio_callback(struct device *port, struct gpio_callback *cb, uint32_t pins) {
+void ModeSwitch::sw1_gpio_callback(const struct device *port, struct gpio_callback *cb, uint32_t pins) {
 	(void)port;
 	(void)pins;
-	ModeSwitch *ms = CONTAINER_OF(cb, ModeSwitch, sw0_cb_data);
-	ms->gpio_callback();
+	ModeSwitch *ms = CONTAINER_OF(cb, ModeSwitch, sw1_cb_data);
+	// Debounce the switch by waiting for some time before calling the
+	// callback.
+	k_work_schedule(&ms->callback, DEBOUNCE_INTERVAL);
 }
 
-void ModeSwitch::gpio_callback() {
-	// TODO
+void ModeSwitch::static_on_callback(struct k_work *work) {
+	ModeSwitch *thisptr = CONTAINER_OF(k_work_delayable_from_work(work),
+	                                   ModeSwitch,
+	                                   callback);
+	if (thisptr->callback_fn) {
+		k_sched_lock();
+		thisptr->callback_fn();
+		k_sched_unlock();
+	}
 }
 
--- firmware/main/src/mode_switch.hpp
+++ firmware/main/src/mode_switch.hpp
@@ -21,6 +21,9 @@ class ModeSwitch {
 public:
 	ModeSwitch();
 	/// Destructor which prepares the mode switch for System OFF mode.
+	///
+	/// If the change callback is enabled, it may still be called during
+	/// execution of the destructor.
 	~ModeSwitch();
 
 	/// Returns the selected mode.
@@ -28,21 +31,29 @@ public:
 	/// Returns the selected profile.
 	KeyboardProfile get_profile();
 
-	// Sets a callback which is called whenever the state of the mode
-	// switch changes.
-	//
-	// The callback is called from an interrupt handler. The previous
-	// callback may still be called during the call to this function, but
-	// will never be called after this function returns. Similarly, the
-	// destructor may still call the callback registered at the time.
+	/// Sets a callback which is called whenever the state of the mode
+	/// switch changes.
+	///
+	/// The callback is called from the system workqueue while preemption is
+	/// disabled. The previous callback will never be called after this
+	/// function returns.
 	void set_callback(void (*change_callback)());
 private:
-	static void sw0_gpio_callback(struct device *port, struct gpio_callback *cb, uint32_t pins);
-	static void sw1_gpio_callback(struct device *port, struct gpio_callback *cb, uint32_t pins);
-	void gpio_callback();
+	unsigned int get_position();
+
+	static void sw0_gpio_callback(const struct device *port, struct gpio_callback *cb, uint32_t pins);
+	static void sw1_gpio_callback(const struct device *port, struct gpio_callback *cb, uint32_t pins);
+
+	static void static_on_callback(struct k_work *work);
 
 	struct gpio_callback sw0_cb_data;
 	struct gpio_callback sw1_cb_data;
+
+	const struct device *sw0_gpio;
+	const struct device *sw1_gpio;
+
+	struct k_work_delayable callback;
+	void (*callback_fn)();
 };
 
 #endif
