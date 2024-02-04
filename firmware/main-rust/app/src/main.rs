17e722a87007b2c9b8a8050b667e1685cf4e7d80
Mathias Gottschlag <mgottschlag@gmail.com>
2024-02-04 19:55:55 +0100

    Draft code for power management/application structure.

--- firmware/main-rust/app/src/main.rs
+++ firmware/main-rust/app/src/main.rs
@@ -1,3 +1,19 @@
+//! Firmware for the "Goboard" custom keyboard (hardware-specific parts).
+//!
+//! # Structure
+//!
+//! The firmware is written using the `embassy` framework. For Bluetooth Low Energy, it uses the
+//! `softdevice` runtime. This structure means that different code has to be executed at different
+//! priorities, so two different embassy executors are used. The main executor runs at a low
+//! priority and implements everything except for the Unifying radio protocol. This protocol is
+//! implemented using the timeslot API of softdevice so that BLE and Unifying can coexist.
+//! Therefore, the Unifying implementation has strict realtime requirements (it needs to complete
+//! its tasks before its timeslot expires), so the code is executed in an interrupt executor at a
+//! higher priority (see `run_esb()`).
+//!
+//! This structure impacts both data flow as well as overall control flow. Events are passed to the
+//! Unifying code and back using embassy channels and the shutdown/low power logic makes sure that
+//! the Unifying code has finished before shutting the system down via Softdevice.
 #![no_std]
 #![no_main]
 #![allow(incomplete_features)]
@@ -5,6 +21,8 @@
 #![macro_use]
 
 mod key_matrix;
+//mod power;
+//mod usb;
 
 use defmt_rtt as _; // global logger
 use embassy_nrf as _; // time driver
@@ -13,17 +31,19 @@ use panic_probe as _;
 use core::mem;
 
 use cortex_m_rt::entry;
-use defmt::{info, unwrap};
-use embassy_nrf::{pwm, Peripherals};
-use embassy_sync::channel::Channel;
+use defmt::{error, info, unwrap};
 use embassy_executor::{Executor, InterruptExecutor};
 use embassy_nrf::interrupt;
 use embassy_nrf::interrupt::InterruptExt;
+use embassy_nrf::{pwm, wdt, Peripherals};
+use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
+use embassy_sync::channel::{Channel, Receiver, Sender};
 use nrf_softdevice::{raw, Softdevice};
 use static_cell::StaticCell;
 
 use key_matrix::KeyMatrix;
-use keyboard::keys::Keys;
+use keyboard::dispatcher::{RadioInEvent, RadioOutEvent};
+use keyboard::keys::{Keys, KeysOutEvent};
 use keyboard::Timer;
 
 struct EmbassyTimer;
@@ -34,52 +54,131 @@ impl Timer for EmbassyTimer {
     }
 }
 
+/// Parts of the firmware that interact with ESB (i.e., Unifying protocol implementation).
 #[embassy_executor::task]
-async fn run_esb() {
+async fn run_esb(
+    input: Receiver<'static, CriticalSectionRawMutex, RadioInEvent, 1>,
+    output: Sender<'static, CriticalSectionRawMutex, RadioOutEvent, 1>,
+) {
+    // TODO: Dummy code
+    /*match event {
+        RadioInEvent::KeysChanged(key_state) => {}
+        RadioInEvent::PowerTransition(power_level) => {
+            // TODO
+        }
+    }*/
     // TODO
 }
 
 #[embassy_executor::task]
-async fn run_main(p: Peripherals) {
-    let input = Channel::new();
-    let output = Channel::new();
-    let key_matrix = KeyMatrix::new(p.P0_22, p.P0_12, p.P0_07, p.SPI3, p.P1_00, p.P0_05, p.P0_04);
-    let mut keys = Keys::new(key_matrix, input.receiver(), output.sender()).await;
-
-    let keys_function = async {
-        keys.run(&EmbassyTimer {}).await;
-    };
-
-    // TODO: Experimental code.
-    //let seq_words: [u16; 8] = [0, 500, 750, 875, 932, 875, 750, 500];
-    let mut seq_words = [1000u16; 128];
-    let mut brightness = 1000;
-    for i in 0..64 {
-        seq_words[i] = 1000 - brightness;
-        brightness = brightness * 10 / 11;
-    }
-    for i in 64..128 {
-        seq_words[i] = seq_words[128 - i - 1];
-    }
-
-    let mut config = pwm::Config::default();
-    config.prescaler = pwm::Prescaler::Div128;
-    let mut seq_config = pwm::SequenceConfig::default();
-    seq_config.refresh = 4; // 8ms per period => 4*8 = 32ms per step in the sequence.
-
-    let mut pwm = unwrap!(pwm::SequencePwm::new_1ch(p.PWM0, p.P1_03, config,));
-
-    let sequencer = pwm::SingleSequencer::new(&mut pwm, &seq_words, seq_config);
-    unwrap!(sequencer.start(pwm::SingleSequenceMode::Infinite));
-
-    let print_function = async {
-        loop {
-            let event = output.recv().await;
-            info!("key event: {:?}", event);
+async fn run_main(
+    p: Peripherals,
+    unifying_input: Sender<'static, CriticalSectionRawMutex, RadioInEvent, 1>,
+    unifying_output: Receiver<'static, CriticalSectionRawMutex, RadioOutEvent, 1>,
+) {
+    // We want a watchdog timer here that resets the system when there is a panic.
+    // TODO: Actually feed the watchdog!
+    let mut config = wdt::Config::default();
+    config.timeout_ticks = 2 << 16; // The watchdog should be fed once per two seconds.
+    config.run_during_debug_halt = false;
+    let (_wdt, [_handle]) = match wdt::Watchdog::try_new(p.WDT, config) {
+        Ok(x) => x,
+        Err(_) => {
+            error!("Watchdog already active with wrong config.");
+            loop {}
         }
     };
 
-    embassy_futures::join::join(keys_function, print_function).await;
+    {
+        let input = Channel::new();
+        let output = Channel::new();
+        let key_matrix =
+            KeyMatrix::new(p.P0_22, p.P0_12, p.P0_07, p.SPI3, p.P1_00, p.P0_05, p.P0_04);
+        let mut keys = Keys::new(key_matrix, input.receiver(), output.sender()).await;
+
+        let keys_function = async {
+            keys.run(&EmbassyTimer {}).await;
+        };
+
+        // TODO: Experimental code.
+        //let seq_words: [u16; 8] = [0, 500, 750, 875, 932, 875, 750, 500];
+        let mut seq_words = [1000u16; 128];
+        let mut brightness = 1000;
+        for i in 0..64 {
+            seq_words[i] = 1000 - brightness;
+            brightness = brightness * 10 / 11;
+        }
+        for i in 64..128 {
+            seq_words[i] = seq_words[128 - i - 1];
+        }
+
+        let mut config = pwm::Config::default();
+        config.prescaler = pwm::Prescaler::Div128;
+        let mut seq_config = pwm::SequenceConfig::default();
+        seq_config.refresh = 4; // 8ms per period => 4*8 = 32ms per step in the sequence.
+
+        let mut pwm = unwrap!(pwm::SequencePwm::new_1ch(p.PWM0, p.P1_03, config,));
+
+        let sequencer = pwm::SingleSequencer::new(&mut pwm, &seq_words, seq_config);
+        unwrap!(sequencer.start(pwm::SingleSequenceMode::Infinite));
+
+        let print_function = async {
+            loop {
+                let event = output.receive().await;
+                match event {
+                    KeysOutEvent::KeysChanged(keys) => {
+                        info!("keys pressed:");
+                        for key in keys.into_iter() {
+                            info!("  {:?}", key);
+                        }
+                        // TODO
+                    }
+                    other => {
+                        info!("other key event: {:?}", other);
+                    }
+                }
+            }
+        };
+
+        embassy_futures::join::join(keys_function, print_function).await;
+    }
+
+    // When we reach this point, the keyboard is supposed to be shut down (either because the
+    // batteries are empty or because no connection is selected). In this situation, most of the
+    // peripherals have already been reconfigured for low-power operation in the drop() method of
+    // the corresponding types, but we still need to perform a number of additional steps to ensure
+    // that we are able to transition into a low-power state:
+    //
+    // 1. We stop the ESB task and wait until it is finished. This step ensures that the radio is
+    //    in a known good state when we shut the softdevice down. TODO: Are there even situations
+    //    where the ESB task has not been stopped by the dispatcher yet? Can we ensure that
+    //    timeouts during communication with the ESB task are caught via a watchdog?
+    // 2. We manually configure all GPIOs so that overall power consumption is minimized and
+    //    changes to the relevant inputs (mode switch, USB voltage) trigger a reset.
+    // 3. Switch to System OFF mode to conserve power.
+
+    // TODO: Stop the ESB task. May not actually be needed, see above.
+
+    // TODO: Configure the GPIOs.
+
+    // Switch to system OFF mode.
+    //unsafe { nrf_softdevice_s140::sd_power_system_off() };
+
+    // If for some reason we cannot switch to system OFF mode, we use the watchdog timer to perform
+    // a system reset. One exception is debug mode in which case we do not want the watchdog to
+    // fire but instead want to block the whole system here so that the debugger remains attached.
+
+    /*let mut config = wdt::Config::default();
+    config.timeout_ticks = 1; // The actual timeout will be slightly higher.
+    config.run_during_debug_halt = false;
+    let (_wdt, [_handle]) = match wdt::Watchdog::try_new(p.WDT, config) {
+        Ok(x) => x,
+        Err(_) => {
+            error!("Watchdog already active with wrong config.");
+            loop {}
+        }
+    };*/
+    loop {}
 }
 
 static ESB_EXECUTOR: InterruptExecutor = InterruptExecutor::new();
@@ -137,6 +236,9 @@ fn main() -> ! {
     };
     let _sd = Softdevice::enable(&config);
 
+    static UNIFYING_INPUT: Channel<CriticalSectionRawMutex, RadioInEvent, 1> = Channel::new();
+    static UNIFYING_OUTPUT: Channel<CriticalSectionRawMutex, RadioOutEvent, 1> = Channel::new();
+
     // As softdevice is always active, ESB RF code (e.g., Unifying) needs to use the timeslot API.
     // As that API has strict realtime requirements, we run it in a separate executor with a higher
     // priority. The softdevice uses IRQ priorities 0, 1, and 4, so we run this code at priority 5.
@@ -147,18 +249,12 @@ fn main() -> ! {
     // Everything else is executed in thread mode.
     let executor = NORMAL_EXECUTOR.init(Executor::new());
     executor.run(|spawner| {
-        unwrap!(spawner.spawn(run_main(peripherals)));
+        unwrap!(spawner.spawn(run_main(
+            peripherals,
+            UNIFYING_INPUT.sender(),
+            UNIFYING_OUTPUT.receiver()
+        )));
     });
-
-    // We need to perform an orderly shutdown to bring everything into a state where minimal power
-    // is consumed but the system is able to wake up from System OFF mode when the mode selector
-    // switch is moved. In the complex structure of this firmware (two embassy executors +
-    // softdevice), such a shutdown is rather complex. We perform the following steps:
-    // 1. Stop the ESB task and wait until it is finished.
-    // 2. Disable the softdevice. At this point, we have control over all peripherals.
-    // 3. Instruct all tasks of the normal executor to finish and to restore their peripherals to a
-    //    state suitable for low-power operation. This includes configuring wakeup sources.
-    // 4. Switch to System OFF mode to conserve power.
 
     // The firmware is structured as a pipeline:
     // - The key matrix module scans the key matrix, performs debouncing, and outputs its state
